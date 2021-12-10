3c808050b1eb51613341fa758de5c5b96adc2751
= <liam2robertson@gmail.com>
2021-12-10 17:17:11 +0000

    💄 set up core styles

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,56 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+@font-face {
+  font-family: "Work Sans";
+  src: url("/font/WorkSans-Subset.woff2") format("woff2");
+  font-style: normal;
+  font-weight: 1 999;
+  font-display: swap;
+}
+
+$color-bg: #1a1b1b;
+
+:root {
+  --padding-page: 0.5rem;
+
+  --color-text: white;
+  --color-bg: #{$color-bg};
+}
+
+body {
+  padding: var(--padding-page);
+  margin: 0;
+  font-family: "Work Sans", sans-serif;
+  background-color: var(--color-bg);
+  color: var(--color-text);
+}
+
+p,
+ul,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: 600;
+}
+
+ul {
+  padding-left: 1rem;
+}
+
+* {
+  box-sizing: border-box;
+}
 </style>
